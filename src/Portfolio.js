d3b54c875a7b8517cfe719e31e77c23beae6581a
cam273364 <cam273364@gmail.com>
2023-06-11 11:55:06 -0400

    added lesson-ease project

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,6 +3,7 @@ import ideation from './ideation.jpg'
 import pokedex from './pokedex.jpg'
 import nutricipe from './nutricipe.jpg'
 import noteTaker from './note-taker.jpg'
+import lessonEase from './lesson-ease.jpg'
 
 function Portfolio() {
 
@@ -30,7 +31,13 @@ function Portfolio() {
             githubRepo: 'https://github.com/cam273364/note-taker-app',
             screenshot: noteTaker,
             name: "note-taker"
-        }
+        },
+        { 
+            deployedUrl: 'https://lesson-ease-client.herokuapp.com/',
+            githubRepo:    'https://github.com/cam273364/lesson-ease-react',
+            screenshot: lessonEase,
+            name: "ideation"
+          },
     ]
 
     return (
